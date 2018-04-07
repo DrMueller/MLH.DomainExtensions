bb85907c3e107af63ee3378e95b7ef0fd886b208
Matthias Mueller <matthiasm@live.de>
2018-04-07 22:44:20 +0200

    WIP

---
+++ Sources/Application/Areas/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mmu.Mlh.DomainExtensions.Areas.DomainModeling;
+
+namespace Mmu.Mlh.DomainExtensions.Areas.Repositories
+{
+    public interface IRepository<T>
+        where T : AggregateRoot
+    {
+        Task DeleteAsync(string id);
+
+        Task<IReadOnlyCollection<T>> LoadAllAsync();
+
+        Task<T> LoadByIdAsync(string id);
+
+        Task<T> SaveAsync(T aggregateRoot);
+    }
+}
